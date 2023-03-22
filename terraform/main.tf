133bd4f71dc35501e838da6af529673467c6b317
Aleksandr Gerasimov <admin@wowteam.ru>
2023-03-22 13:20:51 +0300

    Выполнено ДЗ по terraform-1

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,8 @@ terraform {
   required_providers {
     yandex = {
       source  = "yandex-cloud/yandex"
-      version = "0.87.0"
+      //version = "0.87.0"
+      version = "~> 0.35"
     }
   }
 }
