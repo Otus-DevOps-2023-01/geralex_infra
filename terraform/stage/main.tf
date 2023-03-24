4cf66ad0d101a7d97cf0fd820d6aa4db25b04f01
Aleksandr Gerasimov <admin@wowteam.ru>
2023-03-24 14:06:59 +0300

    Выполнено ДЗ по terraform-2

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -1,10 +1,12 @@
 terraform {
   required_providers {
     yandex = {
-      source  = "yandex-cloud/yandex"
-      version = "0.87.0"
+      source = "yandex-cloud/yandex"
+      #      version = "0.87.0"
     }
+    required_version = ">= 0.13"
   }
+
 
   backend "s3" {
     endpoint   = "storage.yandexcloud.net"
